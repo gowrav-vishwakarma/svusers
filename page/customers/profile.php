880ef53b239e1705b7f81218c1622f7986f1cffa
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2013-04-10 09:53:04 +0530

    Updated profile page , removed username, name from form

--- page/customers/profile.php
+++ page/customers/profile.php
@@ -6,7 +6,7 @@ class page_customers_profile extends page_customer{
 
 		$form=$this->add('Form');
 		// $customer=$this->add('Model_Customer');
-		$form->setModel($this->api->auth->model);
+		$form->setModel($this->api->auth->model,array('Address','Occupation','Company_Name','Company_Address','PAN_No','Contact_No','Email_Id','CoApplicant_Name','CoApplicant_Occupation','CoApplicant_Address','CoApplicant_CompanyName','CoApplicant_Designation','CoApplicantCompanyAddress','CoApplicant_PAN_No','CoApplicant_Contact_No','CoApplicant_Email_Id'));
 		$form->addField('password','password');
 		$form->addField('password','repassword');
 		$form->addSubmit('Upadate');
